75d12ee7831fe7f6ae2ee2b74bc9c9d03b7c0908
wuzhengxing <873094117@qq.com>
2017-03-03 12:21:56 +0800

    弹幕的头像昵称属性拓展及显示

--- app/src/main/java/cn/ucai/live/ui/widget/BarrageLayout.java
+++ app/src/main/java/cn/ucai/live/ui/widget/BarrageLayout.java
@@ -16,6 +16,8 @@ import cn.ucai.live.R;
 import com.github.florent37.viewanimator.AnimationBuilder;
 import com.github.florent37.viewanimator.AnimationListener;
 import com.github.florent37.viewanimator.ViewAnimator;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+import com.hyphenate.easeui.widget.EaseImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -79,20 +81,22 @@ public class BarrageLayout extends LinearLayout {
 
     }
 
-    public synchronized void addBarrage(String msgContent, String username) {
+    public synchronized void addBarrage(String msgContent, String username,String usernick) {
         int i = count % 2;
         Message message = handler.obtainMessage();
         message.what = i;
-        message.obj = newBarrageView(msgContent, username);
+        message.obj = newBarrageView(msgContent, username,usernick);
         handler.sendMessage(message);
         count++;
     }
 
-    private View newBarrageView(String msgContent, String username){
+    private View newBarrageView(String msgContent, String username,String usernick){
         View barrageView = LayoutInflater.from(getContext()).inflate(R.layout.layout_barrage_show, null);
         TextView nameView = (TextView) barrageView.findViewById(R.id.name);
+        EaseImageView avatar = (EaseImageView) barrageView.findViewById(R.id.avatar);
         TextView contentView = (TextView) barrageView.findViewById(R.id.content);
-        nameView.setText(username);
+        EaseUserUtils.setAppUserAvatar(getContext(),username,avatar);
+        nameView.setText(usernick);
         contentView.setText(msgContent);
         return barrageView;
     }
