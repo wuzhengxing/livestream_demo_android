77310b8d883622cc761ecc3c928aa56de918956d
wuzhengxing <873094117@qq.com>
2017-03-03 15:01:57 +0800

    打赏礼物列表的自定义view

--- app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
@@ -441,6 +441,11 @@ public abstract class LiveBaseActivity extends BaseActivity {
   }
 
   @OnClick(R.id.present_image) void onPresentImageClick() {
+    final RoomGiftListDialog dialog =
+            RoomGiftListDialog.newInstance();
+    dialog.show(getSupportFragmentManager(), "RoomGiftListDialog");
+  }
+  private void sendPresent(){
     EMMessage message = EMMessage.createSendMessage(EMMessage.Type.CMD);
     message.setReceipt(chatroomId);
     EMCmdMessageBody cmdMessageBody = new EMCmdMessageBody(LiveConstants.CMD_GIFT);
